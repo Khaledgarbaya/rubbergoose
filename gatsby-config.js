5c072d3839bd050218150fc3b6d2b6ccddbcbb3e
Khaled Garbaya <khaledgarbaya@gmail.com>
2020-01-21 22:45:09 +0100

    Add Stripe processing

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,13 +39,6 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`,
       },
     },
-    {
-      resolve: `gatsby-source-stripe`,
-      options: {
-        objects: ["Product", "Sku", "Subscription"],
-        secretKey: process.env.STRIPE_SECRET,
-      },
-    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
