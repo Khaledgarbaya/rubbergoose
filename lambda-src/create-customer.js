d76933fd908a3e7706195a02d084b45145b8bd6d
Khaled Garbaya <khaledgarbaya@gmail.com>
2020-01-21 23:10:24 +0100

    Fix stripe Syntax error

--- lambda-src/create-customer.js
+++ lambda-src/create-customer.js
@@ -1,41 +1,36 @@
-const statusCode = 200;
+const statusCode = 200
 const headers = {
-  "Access-Control-Allow-Origin" : "*",
-  "Access-Control-Allow-Headers": "Content-Type"
-};
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Headers": "Content-Type",
+}
 // Set your secret key: remember to change this to your live secret key in production
 // See your keys here: https://dashboard.stripe.com/account/apikeys
-const stripe = require('stripe')('sk_test_XUeSJEOdMyDuVnLU8ZItdUJn00VJVU9Iy1');
+const stripe = require("stripe")("sk_test_XUeSJEOdMyDuVnLU8ZItdUJn00VJVU9Iy1")
 
 exports.handler = async function(event, context, callback) {
   //-- We only care to do anything if this is our POST request.
-  if(event.httpMethod !== 'POST' || !event.body) {
+  if (event.httpMethod !== "POST" || !event.body) {
     callback(null, {
       statusCode,
       headers,
-      body: ''
-    });
+      body: "",
+    })
   }
 
-    //-- Parse the body contents into an object.
-    const data = JSON.parse(event.body);
-    //-- Make sure we have all required data. Otherwise, escape.
-    if(
-      !data.email ||
-      !data.payment_method ||
-    ) {
+  //-- Parse the body contents into an object.
+  const data = JSON.parse(event.body)
+  //-- Make sure we have all required data. Otherwise, escape.
+  if (!data.email || !data.payment_method) {
+    console.error("Required information is missing.")
 
-      console.error('Required information is missing.');
+    callback(null, {
+      statusCode,
+      headers,
+      body: JSON.stringify({ status: "missing-information" }),
+    })
 
-      callback(null, {
-        statusCode,
-        headers,
-        body: JSON.stringify({status: 'missing-information'})
-      });
-
-      return;
-    }
-
+    return
+  }
 
   // This creates a new Customer and attaches the PaymentMethod in one API call.
   const customer = await stripe.customers.create({
@@ -44,10 +39,10 @@ exports.handler = async function(event, context, callback) {
     invoice_settings: {
       default_payment_method: data.payment_method,
     },
-  });
+  })
   callback(null, {
     statusCode,
     headers,
-    body: JSON.stringify({status})
-  });
+    body: JSON.stringify({ status }),
+  })
 }
