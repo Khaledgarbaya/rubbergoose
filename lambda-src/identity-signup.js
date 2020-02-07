717e61e060a0e80d30a2274343ec668b3f8aeb36
Khaled Garbaya <khaledgarbaya@gmail.com>
2020-02-07 21:43:18 +0100

    debuggging identity-signup

--- lambda-src/identity-signup.js
+++ lambda-src/identity-signup.js
@@ -3,34 +3,39 @@ const fetch = require("node-fetch")
 export async function handler(event, context) {
   const { user } = JSON.parse(event.body)
   console.log(user)
+  const responseBodyString = JSON.stringify({
+    query: `
+  mutation insertUser($id: String, $email:String, $name:String){
+    insert_users(objects: {id: $id, email: $email, name: $name}) {
+      affected_rows
+    }
+  }
+  `,
+    variables: {
+      id: user.id,
+      email: user.email,
+      name: user.user_metadata.full_name,
+    },
+  })
+
+  const result = await fetch("https://rubbergoose.herokuapp.com/v1/graphql", {
+    method: "POST",
+    body: responseBodyString,
+    headers: {
+      "Content-Type": "application/json",
+      "x-hasura-admin-secret": process.env.HASURA_SECRET,
+    },
+  })
+  const { errors, data } = await result.json()
+  console.log("data====================")
+  console.log(data)
+  console.log("data====================")
+  console.log("errros====================")
+  console.log(errors)
+  console.log("errros====================")
   return {
     statusCode: 200,
   }
-  //const responseBodyString = JSON.stringify({
-  //query: `
-  //mutation insertUser($id: String, $email:String, $name:String){
-  //insert_users(objects: {id: $id, email: $email, name: $name}) {
-  //affected_rows
-  //}
-  //}
-  //`,
-  //variables: {
-  //id: user.id,
-  //email: user.email,
-  //name: user.user_metadata.full_name,
-  //},
-  //})
-
-  //const result = await fetch("https://rubbergoose.herokuapp.com/v1/graphql", {
-  //method: "POST",
-  //body: responseBodyString,
-  //headers: {
-  //"Content-Type": "application/json",
-  //"x-hasura-admin-secret": process.env.HASURA_SECRET,
-  //},
-  //})
-  //const { errors, data } = await result.json()
-
   //if (errors) {
   //console.log(errors)
   //return {
